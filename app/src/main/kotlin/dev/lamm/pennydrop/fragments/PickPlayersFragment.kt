e72ffac8adcd07bd8b2459a0d7ada865fb2ea8a4
Dennis Lamm <dennis.lamm@pm.me>
2022-11-25 14:40:06 +0100

    feat(pick-players): add player selection screen with DataBinding
    PickPlayersFragment + PickPlayersViewModel manage up to six rows of
    players (human/AI toggle, name, include checkbox). Custom binding
    adapters drive the AI toggle background and other two-way bindings.

---
+++ app/src/main/kotlin/dev/lamm/pennydrop/fragments/PickPlayersFragment.kt
@@ -0,0 +1,48 @@
+package dev.lamm.pennydrop.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import dev.lamm.pennydrop.R
+import dev.lamm.pennydrop.databinding.FragmentPickPlayersBinding
+import dev.lamm.pennydrop.viewmodels.GameViewModel
+import dev.lamm.pennydrop.viewmodels.PickPlayersViewModel
+import kotlinx.coroutines.launch
+
+class PickPlayersFragment : Fragment() {
+
+    private val pickPlayersViewModel by activityViewModels<PickPlayersViewModel>()
+    private val gameViewModel by activityViewModels<GameViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentPickPlayersBinding
+            .inflate(inflater, container, false)
+            .apply {
+                this.vm = pickPlayersViewModel
+
+                this.buttonPlayGame.setOnClickListener {
+                    viewLifecycleOwner.lifecycleScope.launch {
+                        gameViewModel.startGame(
+                            pickPlayersViewModel.players.value?.filter { newPlayer ->
+                                newPlayer.isIncluded.get()
+                            }?.map { newPlayer ->
+                                newPlayer.toPlayer()
+                            } ?: emptyList()
+                        )
+
+                        findNavController().navigate(R.id.gameFragment)
+                    }
+                }
+            }
+
+        return binding.root
+    }
+}
---
+++ app/src/main/kotlin/dev/lamm/pennydrop/types/NewPlayer.kt
@@ -0,0 +1,38 @@
+package dev.lamm.pennydrop.types
+
+import androidx.databinding.ObservableBoolean
+import dev.lamm.pennydrop.game.AI
+
+data class NewPlayer(
+    var playerName: String = "",
+    val isHuman: ObservableBoolean = ObservableBoolean(true),
+    val canBeRemoved: Boolean = true,
+    val canBeToggled: Boolean = true,
+    var isIncluded: ObservableBoolean = ObservableBoolean(!canBeRemoved),
+    var selectedAIPosition: Int = -1
+) {
+    fun selectedAI() = if (!isHuman.get()) {
+        AI.basicAI.getOrNull(selectedAIPosition)
+    } else {
+        null
+    }
+
+    fun toPlayer() = Player(
+        playerName =
+            if (this.isHuman.get()) this.playerName
+            else (this.selectedAI()?.name ?: "AI"),
+        isHuman = this.isHuman.get(),
+        selectedAI = this.selectedAI()
+    )
+
+    override fun toString() = listOf(
+        "name" to this.playerName,
+        "isIncluded" to this.isIncluded.get(),
+        "isHuman" to this.isHuman.get(),
+        "canBeRemoved" to this.canBeRemoved,
+        "canBeToggled" to this.canBeToggled,
+        "selectedAI" to (this.selectedAI()?.name ?: "N/A")
+    ).joinToString(", ", "NewPlayer(", ")") { (property, value) ->
+        "$property=$value"
+    }
+}
