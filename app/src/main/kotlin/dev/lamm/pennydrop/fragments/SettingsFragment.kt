08a2e446be15601908d398247504ec9f05d110b1
Dennis Lamm <dennis.lamm@pm.me>
2022-12-06 13:54:09 +0100

    feat(settings): add settings and About screens
    Settings exposes a PreferenceScreen for starting penny count, fast-AI
    toggle, app theme, and dark-mode override. AboutFragment lists credits
    and icon attribution.

---
+++ app/src/main/kotlin/dev/lamm/pennydrop/fragments/SettingsFragment.kt
@@ -0,0 +1,49 @@
+package dev.lamm.pennydrop.fragments
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.fragment.findNavController
+import androidx.preference.DropDownPreference
+import androidx.preference.ListPreference
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import dev.lamm.pennydrop.R
+
+
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(
+        savedInstanceState: Bundle?,
+        rootKey: String?
+    ) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        val themePreference = findPreference<DropDownPreference?>("theme")
+        val themeModePreference = findPreference<ListPreference?>("themeMode")
+        val creditsPreference = findPreference<Preference?>("credits")
+
+        themePreference?.onPreferenceChangeListener =
+            Preference.OnPreferenceChangeListener { _, _ ->
+                activity?.recreate()
+
+                true
+            }
+
+        themeModePreference?.onPreferenceChangeListener =
+            Preference.OnPreferenceChangeListener { _, newValue ->
+                val nightMode = when (newValue?.toString()) {
+                    "Light" -> AppCompatDelegate.MODE_NIGHT_NO
+                    "Dark" -> AppCompatDelegate.MODE_NIGHT_YES
+                    else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                }
+
+                AppCompatDelegate.setDefaultNightMode(nightMode)
+                true
+            }
+
+        creditsPreference?.onPreferenceClickListener =
+            Preference.OnPreferenceClickListener { _ ->
+                this.findNavController().navigate(R.id.aboutFragment)
+                true
+            }
+    }
+}
